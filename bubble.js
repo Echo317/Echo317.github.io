005e636a58c69168e17b076aa44632216bfc0ef6
Echo317 <echo_317@126.com>
2019-01-15 14:47:45 +0800

    Update bubble.js

--- bubble.js
+++ bubble.js
@@ -1,4 +1,4 @@
-define(["https://ajax.googleapis.com/ajax/libs/d3js/5.7.0/d3.min.js"], function( d3 ) {
+define(["https://cdnjs.cloudflare.com/ajax/libs/d3/5.7.0/d3.min.js"], function( d3 ) {
 "use strict";
 
 function D3BarChart()
